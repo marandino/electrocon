97a99089b17ccd6c57875e249d810412bb629116
Chiyen Venegas <chi@marandino.dev>
2021-03-13 19:40:19 +0000

    implement carousel in home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
-    <HelloWorld msg="Default Boilerplate Template" />
-  </div>
+  <carousel />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Carousel: require("../components/Carousel.vue").default
   }
 };
 </script>
