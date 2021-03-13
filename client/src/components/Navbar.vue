3669efd70884795bdcb99e89ecf76fdb043d8292
Chiyen Venegas <chi@marandino.dev>
2021-03-13 19:40:03 +0000

    replace logo

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,8 +3,8 @@
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
+          src="../assets/logo.png"
+          alt="Electrocon Systems Logo"
         />
       </b-navbar-item>
     </template>
