f8000b2b3a12a955ceae15504600853bb6bc1359
Chiyen Venegas <chi@marandino.dev>
2021-03-14 12:54:45 +0000

    implement tawk

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,6 +17,22 @@
 </template>
 
 <script>
+// - TAWK
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/604d606b067c2605c0b81890/1f0n41970";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
+
+console.log(Tawk_LoadStart, "tawk has been loaded");
+//- End of TAWK
+
 export default {
   components: {
     Navbar: require("./components/Navbar.vue").default
