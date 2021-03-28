61c25490fcab9546fb6095da69a6b0ee30c2f279
Chiyen Venegas <chi@marandino.dev>
2021-03-28 15:25:28 +0100

    app|add: prismic.io

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,17 @@ import router from "./router";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 
+// start of Prismic.io
+import PrismicVue from "@prismicio/vue";
+import linkResolver from "./link-resolver.js";
+
+Vue.use(PrismicVue, {
+  endpoint: "https://electrocon.cdn.prismic.io/api/v2",
+  linkResolver
+  // apiOptions: { accessToken }
+});
+// - end of Prismic
+
 Vue.use(Buefy);
 Vue.config.productionTip = false;
 
